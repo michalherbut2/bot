1d7dd7c571d21f8ea3ac908fec92b06e7467014c
Michal <michal@michalarch.local>
2024-05-17 00:52:39 +0200

    send images separetly

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,23 +4,28 @@ const client = require("../index");
 // on send message
 client.on(Events.MessageCreate, async message => {
   // stop if the autor is a bot
-  if (message?.author?.bot) return;
+  // if (message?.author?.bot) return;
 
   // give a reactions to the image in the ADD IMAGES thread
-  if (
-    message.channel?.name?.includes("ADD IMAGES") &&
-    message.attachments.size > 0
-  ) {
-    await message.react("❌");
-    
-    // get all emojis
-    const emojis = await message.guild.emojis.fetch();
+  if (message.channel?.name?.includes("ADD IMAGES")) {
+    if (message.attachments.size > 1) {
+      await Promise.all(
+        message.attachments.map(
+          async a => await message.channel.send({ files: [a.url] })
+        )
+      );
+    } else if (message.attachments.size == 1) {
+      await message.react("❌");
 
-    // get the main page emoji
-    const emoji = emojis.find(e => e.name.toLowerCase() === "mainpage");
+      // get all emojis
+      const emojis = await message.guild.emojis.fetch();
 
-    // give the main page emoji reaction
-    await message.react(emoji.id);
-    // await message.react("1236938634892935190"); // main page emoji
+      // get the main page emoji
+      const emoji = emojis.find(e => e.name.toLowerCase() === "mainpage");
+
+      // give the main page emoji reaction
+      await message.react(emoji.id);
+      // await message.react("1236938634892935190"); // main page emoji
+    }
   }
 });
